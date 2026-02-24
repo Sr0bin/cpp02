fde36c48f9be52d9e6949898162995e99aef65be
Robin Rollin <10898753+Sr0bin@users.noreply.github.com>
2026-02-24 15:12:24 +0100

    [EX00]

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,36 +6,44 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 17:02:12 by rorollin          #+#    #+#             */
-/*   Updated: 2026/02/24 15:06:56 by rorollin         ###   ########.fr       */
+/*   Updated: 2026/02/24 15:11:59 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <iostream>
 
-Fixed::Fixed() : _value(0) {} ;
+Fixed::Fixed() : _value(0) 
+{
+	std::cout << "Default Constructor called\n";
+}
 
 Fixed::~Fixed()
 {
-
+	std::cout << "Default Destructor called\n";
 }
 
 Fixed::Fixed(const Fixed &fixed)
 {
+	std::cout << "Copy Constructor called\n";
 	_value = fixed.getRawBits();
 }
 
 Fixed &Fixed::operator=(const Fixed &fix)
 {
+	std::cout << "Copy Assignement operator called\n";
 	_value = fix.getRawBits();
 	return (*this);
 }
 
 int Fixed::getRawBits() const
 {
+	std::cout << "getRawBits member function called\n";
 	return (_value);
 }
 
 void Fixed::setRawBits(int const raw)
 {
+	std::cout << "setRawBits member function called\n";
 	_value = raw;
 }
